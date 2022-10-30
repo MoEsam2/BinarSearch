895060d629e863327900aee04a34db5406cc296f
Mohamed esam <moesam138@gmail.com>
2022-10-30 21:21:42 +0200

    Initial commit

---
+++ src/com/binarysearch/BinarySearch.java
@@ -0,0 +1,19 @@
+package com.binarysearch;
+
+public class BinarySearch {
+    public int binarysearch(int arr[],int x)
+    {
+        int l=0,h=arr.length-1;
+        while(l<=h)
+        {
+             int m=(l+h)/2;
+             if(x == arr[m])
+                return m;
+             else if (x>arr[m])
+                 l=m+1;
+             else
+                 h=m-1;
+        }
+        return -1;
+    }
+ }
---
+++ src/com/binarysearch/Main.java
@@ -0,0 +1,16 @@
+package com.binarysearch;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BinarySearch obj1=new BinarySearch();
+        int arr[]={ 2, 3, 4, 10, 40 };
+        int x=40;
+        int n=obj1.binarysearch(arr,x);
+        if (n == -1)
+            System.out.println("Element not present");
+        else
+            System.out.println("Element found at "
+                    + "index " + n);
+    }
+}
